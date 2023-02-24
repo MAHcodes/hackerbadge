f2adf283a0ee7e9e48f7e0bae69e3ae537988287
mahcodes <mhmdali102@bk.ru>
2023-02-24 03:33:41 +0200

    fix: image not showing

--- scripts/app.js
+++ scripts/app.js
@@ -11,15 +11,15 @@ document.getElementById("save").addEventListener("click", handleSave);
 
 const handleName = (event) => {
   document.getElementById("name").innerText = event.target.value;
-}
+};
 
 document.getElementById("name-input").addEventListener("input", handleName);
 
 const handleRole = (event) => {
   console.log(event.target.checked);
   const roles = document.getElementById("roles");
-  const images =  document.querySelectorAll("form .role-img");
-  images.forEach(img => {
+  const images = document.querySelectorAll("form .role-img");
+  images.forEach((img) => {
     if (img.dataset.role === event.target.id) {
       if (event.target.checked) {
         const newImage = document.createElement("img");
@@ -28,20 +28,27 @@ const handleRole = (event) => {
         newImage.dataset.role = img.dataset.role;
         roles.appendChild(newImage);
       } else {
-        const image = roles.querySelector(`.role-img[data-role="${img.dataset.role}"]`);
+        const image = roles.querySelector(
+          `.role-img[data-role="${img.dataset.role}"]`
+        );
         image.remove();
       }
     }
-  })
-}
+  });
+};
 
-document.querySelectorAll("input[name='role']").forEach(checkbox => {
+document.querySelectorAll("input[name='role']").forEach((checkbox) => {
   checkbox.addEventListener("change", handleRole);
-})
+});
 
-const handleProf = (event) => {
+const handleProf = () => {
   const profImg = document.getElementById("prof-img");
-  profImg.src = event.target.value;
-}
+  const imgInp = document.getElementById("prof-input");
+
+  const [file] = imgInp.files;
+  if (file) {
+    profImg.src = URL.createObjectURL(file);
+  }
+};
 
 document.getElementById("prof-input").addEventListener("input", handleProf);
